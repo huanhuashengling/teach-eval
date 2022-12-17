972d172c08553d4913be31eb29054eb591669ea6
wenjie.you <shengling_2005@163.com>
2022-12-17 23:55:45 +0800

    enable export log data
    enable export log data

--- app/Http/Controllers/Operator/ReportController.php
+++ app/Http/Controllers/Operator/ReportController.php
@@ -13,16 +13,12 @@ use Config;
 
 class ReportController extends Controller
 {
-    private $tasks;
-    private $usersData;
     public function __construct()
     {
         $this->middleware('can:task list', ['only' => ['index', 'show']]);
         $this->middleware('can:task create', ['only' => ['create', 'store']]);
         $this->middleware('can:task edit', ['only' => ['edit', 'update']]);
         $this->middleware('can:task delete', ['only' => ['destroy']]);
-        // $this->usersData = [];
-        // $this->tasks = [];
     }
 
     /**
@@ -36,73 +32,10 @@ class ReportController extends Controller
         $startDate = isset($request->startDate)?$request->startDate:"";
         $endDate = isset($request->endDate)?$request->endDate:"";;
         
-        // echo $participantTypesId . "  " . $startDate . "  " . $endDate;
+        $tasks = $this->getTasksData($participantTypesId, $startDate, $endDate);
 
-        $from = date('Y-m-d H:i', strtotime($startDate." 00:00:00"));
-        $to = date('Y-m-d H:i', strtotime($endDate." 23:59:00"));
+        $usersData = $this->getUsersData($participantTypesId, $tasks);
 
-        $tasks = (new Task)->newQuery();
-        $tasks->with(["taskType", "participantType"]);
-
-        if (request()->has('search')) {
-            $tasks->where('name', 'Like', '%'.request()->input('search').'%');
-        }
-
-        $tasks->where("participant_types_id", "=", $participantTypesId);
-        // $tasks->whereBetween("happen_at", [$from, $to]);
-        if ($request->startDate && $request->endDate) {
-            $tasks->whereDate('happen_at', '<=', $to);
-            $tasks->whereDate('happen_at', '>=', $from);
-        }
-        
-        if (request()->query('sort')) {
-            $attribute = request()->query('sort');
-            $sort_order = 'ASC';
-            if (strncmp($attribute, '-', 1) === 0) {
-                $sort_order = 'DESC';
-                $attribute = substr($attribute, 1);
-            }
-            $tasks->orderBy($attribute, $sort_order);
-        } else {
-            $tasks->latest();
-        }
-        $tasks= $tasks->get();
-        // var_dump($tasks);
-
-        $usersData = [];
-
-        $users = (new User)->newQuery();
-        $users->with("taskLogs");
-        if(1 == $participantTypesId) {
-            $users->where('is_working', '=', '1');
-        } else if(2 == $participantTypesId) {
-            $users->where('is_working', '=', '1');
-            $users->where('is_member', '=', '1');
-        }
-        $users = $users->get();
-        foreach ($users as $k => $user) {
-            $count = 0;
-            $taskLogs = [];
-            foreach ($tasks as $m => $task) {
-                $taskLog = TaskLog::where("tasks_id", "=", $task->id)
-                                    ->where("users_id", "=", $user->id)->first();
-                if(isset($taskLog)) {
-                    array_push($taskLogs, $taskLog);
-                    $count += $taskLog->eval;
-                } else {
-                    array_push($taskLogs, (object)["eval" => 0]);
-                }
-            }
-            array_push($usersData, (object)["user" => $user, 'taskLogs' => $taskLogs, 'count' => $count]);
-        }
-
-        $this->usersData = $usersData;
-        $this->tasks = $tasks;
-        // echo "hhjhj";
-        // dd($tasks);
-        // return Excel::download(new ReportExport($tasks, $usersData), 'tasks.csv');
-        // Config::set('tasks', $tasks);
-        // Config::set('usersData', $usersData);
         return view('operator.report.index', compact('tasks', 'usersData', 'participantTypesId', 'startDate', 'endDate'));
     }
 
@@ -174,13 +107,80 @@ class ReportController extends Controller
 
     public function export(Request $request) 
     {
-        echo $request->participantTypesId;
-        // $tasks = Config::get('tasks');
-        // echo "asasas";
-        // dd($tasks);
+        $participantTypesId = $request->get("ptId");
+        $startDate = $request->get("startDate");
+        $endDate = $request->get("endDate");
+        
+        $tasks = $this->getTasksData($participantTypesId, $startDate, $endDate);
 
-        // dd($this->participantTypesId);
-        dd($this->tasks);
-        return Excel::download(new ReportExport($this->tasks, $this->usersData), 'tasks.csv');
+        $usersData = $this->getUsersData($participantTypesId, $tasks);
+        return Excel::download(new ReportExport($tasks, $usersData), '汇总数据导出.csv');
+    }
+
+    public function getTasksData($ptId, $startDate, $endDate)
+    {
+        $from = date('Y-m-d H:i', strtotime($startDate." 00:00:00"));
+        $to = date('Y-m-d H:i', strtotime($endDate." 23:59:00"));
+
+        $tasks = (new Task)->newQuery();
+        $tasks->with(["taskType", "participantType"]);
+
+        if (request()->has('search')) {
+            $tasks->where('name', 'Like', '%'.request()->input('search').'%');
+        }
+
+        $tasks->where("participant_types_id", "=", $ptId);
+        // $tasks->whereBetween("happen_at", [$from, $to]);
+        if ($startDate && $endDate) {
+            $tasks->whereDate('happen_at', '<=', $to);
+            $tasks->whereDate('happen_at', '>=', $from);
+        }
+        
+        if (request()->query('sort')) {
+            $attribute = request()->query('sort');
+            $sort_order = 'ASC';
+            if (strncmp($attribute, '-', 1) === 0) {
+                $sort_order = 'DESC';
+                $attribute = substr($attribute, 1);
+            }
+            $tasks->orderBy($attribute, $sort_order);
+        } else {
+            $tasks->latest();
+        }
+        $tasks= $tasks->get();
+
+        return $tasks;
+    }
+
+    public function getUsersData($ptId, $tasks)
+    {
+        $usersData = [];
+
+        $users = (new User)->newQuery();
+        $users->with("taskLogs");
+        if(1 == $ptId) {
+            $users->where('is_working', '=', '1');
+        } else if(2 == $ptId) {
+            $users->where('is_working', '=', '1');
+            $users->where('is_member', '=', '1');
+        }
+        $users = $users->get();
+        foreach ($users as $k => $user) {
+            $count = 0;
+            $taskLogs = [];
+            foreach ($tasks as $m => $task) {
+                $taskLog = TaskLog::where("tasks_id", "=", $task->id)
+                                    ->where("users_id", "=", $user->id)->first();
+                if(isset($taskLog)) {
+                    array_push($taskLogs, $taskLog);
+                    $count += $taskLog->eval;
+                } else {
+                    array_push($taskLogs, (object)["eval" => 0]);
+                }
+            }
+            array_push($usersData, (object)["user" => $user, 'taskLogs' => $taskLogs, 'count' => $count]);
+        }
+
+        return $usersData;
     }
 }
--- resources/views/components/wrapper.blade.php
+++ resources/views/components/wrapper.blade.php
@@ -14,7 +14,7 @@
         		<div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             	<svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
         	</div>
-        	<input type="text"  name="endDate" value="{{$endDate}}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="选择结束日期">
+        	<input type="text" name="endDate" value="{{$endDate}}" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="选择结束日期">
     	</div>
     </div>
 
@@ -37,7 +37,7 @@
 		</div>
 
 		<div>
-			<a href="{{ route('operator.report.export') }}"><button type="button" class="focus:outline-none text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-green-900">表格导出</button></a>
+			<a href="{{ route('operator.report.export') }}?ptId={{ $participantTypesId }}&startDate={{$startDate}}&endDate={{$endDate}}"><button type="button" class="focus:outline-none text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-green-900">表格导出</button></a>
 		</div>
 	</div>
 </form>
